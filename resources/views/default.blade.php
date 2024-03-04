dd7f7e5d370a8e22d7f68dacf46059a208c52dd1
dhayal <dhayal.cluzter@gmail.com>
2024-03-04 11:10:59 +0530

    navigation

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -2,13 +2,26 @@
 <html>
 <head>
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
-    
+    <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Technometers') }}</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-
+<!-- navbar -->
+<x-app-layout>
            
 	<div class="container">
 		@yield('content')
 	</div>
+	</x-app-layout>
 </body>
 </html>
